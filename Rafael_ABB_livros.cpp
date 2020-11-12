97cbc4ecce8e02e5a7277ffc946d915f9205e03a
Rafael Bortolozo <rafaelbortolozo@outlook.com>
2020-11-12 01:23:32 -0300

    Update Rafael_ABB_livros.cpp
    Implementação do Pre-ordem, Ordem e Pos-ordem. Mais uma tentativa fracassada de implementar a remoção de uma ABB.

--- Rafael_ABB_livros.cpp
+++ Rafael_ABB_livros.cpp
@@ -3,6 +3,12 @@
 #include <cstdlib> //necessario para limpar o terminal
 #include <conio.h> //biblioteca de desenvolvimento
 
+
+/* Aluno: Rafael Bortolozo 
+   Arvore binaria de busca
+*/
+
+
 typedef struct sNodo{
 	struct sNodo* left;
 	struct sNodo* right;
@@ -15,6 +21,11 @@ int requestData();					 //solicita codigo do livro
 Nodo* del(Nodo* root, int data);	 //delete livro
 Nodo* search(Nodo* root, int data);  //pesquisa livro
 void printSearch(Nodo*);		     //imprime pesquisa
+Nodo* searchMax(Nodo* root);		 //Procura maior elemento
+void pre_ordem (Nodo* root);		 //Pre-ordem
+void em_ordem (Nodo* root);			 //Em-ordem
+void pos_ordem (Nodo* root);		 //Pos-ordem
+Nodo* test(Nodo* root);				 //carrega teste
 
 main(){
 	Nodo* root= createRoot();
@@ -27,7 +38,11 @@ main(){
         printf("\t1)Inserir\n");
         printf("\t2)Excluir\n");
         printf("\t3)Buscar\n");
-        printf("\t4)Sair\n");
+        printf("\t4)Pre-ordem\n");
+        printf("\t5)Em-ordem\n");
+        printf("\t6)Pos-ordem\n");
+        printf("\t7)TEST\n");
+        printf("\t8)Sair\n");
         
         scanf("%d",&op);
         switch (op){
@@ -47,7 +62,7 @@ main(){
 					if(root!=NULL){
 						printf("\tLivro \"%d\" removido com sucesso!\n\n", data);
 					}else{
-						printf("\tERRO, o livro \"%d\" nao existe!\n\n", data);
+						printf("delete Error", data);
 					}
                     op=0;
 					break;
@@ -59,8 +74,32 @@ main(){
 					printSearch(aux);
 					op=0;
 					break;
+			
+			case 4: system("clear||cls");
+					printf("Pre-ordem: \n\n");
+					pre_ordem(root);
+					op=0;
+					break;
+			
+			case 5: system("clear||cls");
+					printf("Em-ordem: \n\n");
+					em_ordem(root);
+					op=0;
+					break;
 					
-			case 4: break;
+			case 6: system("clear||cls");
+					printf("Pos-ordem: \n\n");
+					pos_ordem(root);
+					op=0;
+					break;
+					
+			case 7: system("clear||cls");
+					printf("Teste carregado! \n\n");
+					root= test(root);
+					op=0;
+					break;		
+			
+			case 8: break;
                
             default:system("clear||cls");
 					op=0;
@@ -97,49 +136,40 @@ Nodo* insert(Nodo *root, int data){
 
 Nodo* del(Nodo* root, int data){
 	
-	/*Nodo* nodo= search(root, data);
-	Nodo* aux;
-	
-	if((root==NULL) || (nodo==NULL)){
-		return NULL;
+	if(root != NULL){
+		if(data > root->data){
+			root->right = del(root, data);
+		}else if(data < root->data){
+			root->left = del(root, data);
+		}else{
+			//elemento encontrado
+			//nodo com 0 filho
+			if(root->left==NULL && root->right==NULL){
+				free(root);
+				return NULL;
+			}
+			
+			//nodo com 1 filho
+			else if(root->left==NULL){
+				Nodo* aux= root->right;
+				free(root);
+				return aux;
+			}else if(root->right==NULL){
+				Nodo* aux= root->left;
+				free(root);
+				return aux;
+			}
+			
+			//nodo com 2 filhos
+			else{
+				Nodo* aux= searchMax(root->left);
+				root->data= aux->data;
+				root->left= del(root->left, aux->data);
+			}
+		}
+		return root;
 	}
-	
-	//se o nodo nao tem filhos, entao ele eh removido e termina o codigo
-	if((nodo->left == NULL) && (nodo->right == NULL)){
-		free(nodo);
-    }else{
-    	
-		//se houver algum filho, o nodo eh deletado e o filho passa a ocupar o lugar dele 
-		if(nodo->right == NULL){
-	        aux = nodo;
-	        printf("%d ", aux->data);
-	        nodo = nodo->left;
-	        printf("%d ", nodo->data);
-	        free(aux);
-	        printf("%d ", aux->data);
-	        getch();
-    	}
-    	
-		else if(nodo->left == NULL){
-		    aux = nodo;
-		    nodo = nodo->right;
-		    free(aux);
-    	}
-		
-		//se tiver os 2 filhos, procura-se o elemento mais a direita, na sub-arvore da esquerda
-		else{
-		    aux = nodo->left;
-		    while(aux->right != NULL){
-		        aux=aux->right;
-        	}
-        	
-        	//troca de valores
-        	nodo->data = aux->data;
-        	aux->data=data;
-        	nodo->left= del(nodo->left, data);
-    	}
-	}
-	return root;*/
+	return NULL;
 }
 
 Nodo* search(Nodo* root, int data){
@@ -176,4 +206,64 @@ void printSearch(Nodo* nodo){
 	}else{
 		printf("\tLivro \"%d\" encontrado!\n\n", nodo->data);
 	}
+}
+
+Nodo* searchMax(Nodo* root){
+    if(root != NULL){
+        Nodo* aux = root;
+        while(aux->right != NULL){
+            aux = aux->right;
+        }
+        return aux;
+    }
+	return NULL;  
+}
+
+void pre_ordem (Nodo* root){
+ 	if(root != NULL){
+	    printf("%d ",root->data);
+		pre_ordem(root->left);
+		pre_ordem(root->right);	    
+ 	}
+}
+
+void em_ordem (Nodo* root){
+ 	if(root != NULL){
+	    em_ordem(root->left);
+	    printf("%d ",root->data);
+		em_ordem(root->right);	    
+ 	}
+}
+
+void pos_ordem (Nodo* root){
+ 	if(root != NULL){
+	    pos_ordem(root->left);
+	    pos_ordem(root->right);
+	    printf("%d ",root->data);
+ 	}
+}
+
+Nodo* test(Nodo* root){
+	//insere 5,12,2,7,3,1,10,8,9,4,6,11
+	//remove 11,8,12,5
+	
+	root= insert(root, 5);
+	root= insert(root, 12);
+	root= insert(root, 2);
+	root= insert(root, 7);
+	root= insert(root, 3);
+	root= insert(root, 1);
+	root= insert(root, 10);
+	root= insert(root, 8);
+	root= insert(root, 9);
+	root= insert(root, 4);
+	root= insert(root, 6);
+	root= insert(root, 11);
+	
+	root= del(root, 11);
+	root= del(root, 8);
+	root= del(root, 12);
+	root= del(root, 5);
+	
+	return root;
 }
